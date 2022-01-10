c8436389dcc1da0d2bf7dce943615ca6cc7872e5
Grady Weyenberg <gradysw@hawaii.edu>
2022-01-09 14:33:36 -1000

    putting the knit into a try()

--- tests.R
+++ tests.R
@@ -1,14 +1,15 @@
-knitr::knit(text=readLines('PS01_solution.Rmd'))
+## execute all code in the solution file
+try(knitr::knit(text=readLines('PS01_solution.Rmd')))
 
 test_that("Preliminaries",{
-  expect_equal(x,6)
+  expect_equal(x, 6)
   expect_vector(favorite_movie, character(), 1)
-  expect_equal(v, c(2,4,6))
+  expect_equal(v, c(2, 4, 6))
 })
 
 test_that("Exercise 1", {
   expect_equal(y, 7)
-  expect_equal(z, x*y)
+  expect_equal(z, x * y)
 })
 
 test_that("Exercise 3", {
