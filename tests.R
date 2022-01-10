33498e706cd165b9a68b4e773089cc9cb3a4a533
Grady Weyenberg <gradysw@hawaii.edu>
2022-01-09 14:10:10 -1000

    move autograder to .github

--- tests.R
+++ tests.R
@@ -1,16 +1,9 @@
 knitr::knit(text=readLines('PS01_solution.Rmd'))
 
 test_that("Preliminaries",{
-
-  expect_true(exists('x'))
   expect_equal(x,6)
-  
-  expect_true(exists('favorite_movie'))
   expect_vector(favorite_movie, character(), 1)
-  
-  expect_true(exists('v'))
   expect_equal(v, c(2,4,6))
-  
 })
 
 test_that("Exercise 1", {
