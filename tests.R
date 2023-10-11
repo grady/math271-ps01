856ac9b21aea49cfc94e8bf05d4e5a27dba3c91e
Grady Weyenberg <gradysw@hawaii.edu>
2023-10-11 00:09:19 -1000

    fixes from template

--- tests.R
+++ tests.R
@@ -1,6 +1,6 @@
 test_that("<5>Solution file exists and knits",{
   expect_true(file.exists("solution.Rmd"))
-  suppressWarnings(expect_error(knitr::knit("solution.Rmd", quiet = TRUE), NA))
+  suppressWarnings(expect_error(knitr::knit("solution.Rmd", quiet = TRUE, envir=globalenv()), NA))
 })
 
 test_that("<1>Preliminaries",{
