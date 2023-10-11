631bc93039fa7b7d8dc41d692e6394bdbc215355
Grady Weyenberg <gradysw@hawaii.edu>
2023-10-11 00:22:28 -1000

    more fiddling with tests and errors

--- tests.R
+++ tests.R
@@ -4,22 +4,20 @@ test_that("<5>Solution file exists and knits",{
 })
 
 test_that("<1>Preliminaries",{
-    expect_no_error(expect_equal(x, 6))
-    expect_no_error(expect_vector(favorite_movie, character(), 1))
-    expect_no_error(expect_equal(expect_error(v,NA), c(2, 4, 6)))
+    expect_error(expect_equal(x, 6), NA)
+    expect_error(expect_vector(favorite_movie, character(), 1), NA)
+    expect_error(expect_equal(expect_error(v,NA), c(2, 4, 6)), NA)
 })
 
 test_that("<1>Exercise 1", {
-  expect_no_error({
-    expect_equal(y, 7)
-    expect_equal(z, x * y)
-  })
+  expect_error(expect_equal(y, 7), NA)
+  expect_error(expect_equal(z, x * y), NA)
 })
 
 test_that("<1>Exercise 3", {
-  expect_no_error(expect_equal(a, 9))
+  expect_error(expect_equal(a, 9), NA)
 })
 
 test_that("<1>Exercise 9", {
-  expect_vector(expect_error(music, NA), character(), 3)
+  expect_error(expect_vector(music, character(), 3), NA)
 })
